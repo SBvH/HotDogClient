cd8c12e6549ad574523977b61e34cde946573ed2
SebastianvonHoesslin <38984291+SebastianvonHoesslin@users.noreply.github.com>
2018-06-27 08:48:58 +0200

    HotDogItem can be stored on server

--- app/src/main/java/com/example/sebas/hotdogclient/MainActivity.java
+++ app/src/main/java/com/example/sebas/hotdogclient/MainActivity.java
@@ -4,14 +4,35 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import static android.graphics.Color.BLACK;
 import static android.graphics.Color.GRAY;
+
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.SettableFuture;
+import com.microsoft.windowsazure.mobileservices.*;
+import com.microsoft.windowsazure.mobileservices.http.NextServiceFilterCallback;
+import com.microsoft.windowsazure.mobileservices.http.OkHttpClientFactory;
+import com.microsoft.windowsazure.mobileservices.http.ServiceFilter;
+import com.microsoft.windowsazure.mobileservices.http.ServiceFilterRequest;
+import com.microsoft.windowsazure.mobileservices.http.ServiceFilterResponse;
+import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
+import com.microsoft.windowsazure.mobileservices.table.TableOperationCallback;
+import com.squareup.okhttp.OkHttpClient;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -55,12 +76,44 @@ public class MainActivity extends AppCompatActivity {
     private double onionPrice = 0.30;
     private double cheesePrice = 0.40;
 
+    private MobileServiceClient mClient;
+    private MobileServiceTable<HotDogItem> mHotDogTable;
+    private ProgressBar mProgressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        try {
+            // Create the Mobile Service Client instance, using the provided
 
+            // Mobile Service URL and key
+            mClient = new MobileServiceClient(
+                    "https://hotdog2.azurewebsites.net",
+                    this).withFilter(new ProgressFilter());
+
+            // Extend timeout from default of 10s to 20s
+            mClient.setAndroidHttpClientFactory(new OkHttpClientFactory() {
+                @Override
+                public OkHttpClient createOkHttpClient() {
+                    OkHttpClient client = new OkHttpClient();
+                    client.setReadTimeout(20, TimeUnit.SECONDS);
+                    client.setWriteTimeout(20, TimeUnit.SECONDS);
+                    return client;
+                }
+            });
+
+            // Get the Mobile Service Table instance to use
+
+            mHotDogTable = mClient.getTable(HotDogItem.class);
+
+
+        } catch (MalformedURLException e) {
+            createAndShowDialog(new Exception("There was an error creating the Mobile Service. Verify the URL"), "Error");
+        } catch (Exception e){
+            createAndShowDialog(e, "Error");
+        }
 
         mHotdogPrice = (TextView) findViewById(R.id.hotdog_price);
         mHotdogPrice.setText(String.format("%.2f", hotdogPrice)+ " €");
@@ -221,23 +274,72 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                HotDogItem hotDogItem = new HotDogItem();
-                hotDogItem.setHotdog(mIsHotDog);
-                hotDogItem.setBbqSauce(mIsBbqSauce);
-                hotDogItem.setKetchup(mIsKetchup);
-                hotDogItem.setMayonnaise(mIsMayonnaise);
-                hotDogItem.setCurry(mIsCurry);
-                hotDogItem.setOnion(mIsOnion);
-                hotDogItem.setCheese(mIsCheese);
-                hotDogItem.setTotalPrice(mTotalPrice);
-                double i = hotDogItem.getTotalPrice();
-                Toast.makeText(MainActivity.this, String.format("%.2f", i) + " €",
-                        Toast.LENGTH_SHORT).show();
+
 
                 showOrderConfirmationDialog();
 
             }
         });
+
+
+    }
+
+    private void createAndShowDialog(final String message, final String title) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage(message);
+        builder.setTitle(title);
+        builder.create().show();
+    }
+
+    private void createAndShowDialog(Exception exception, String title) {
+        Throwable ex = exception;
+        if(exception.getCause() != null){
+            ex = exception.getCause();
+        }
+        createAndShowDialog(ex.getMessage(), title);
+    }
+
+    private class ProgressFilter implements ServiceFilter {
+
+        @Override
+        public ListenableFuture<ServiceFilterResponse> handleRequest(ServiceFilterRequest request, NextServiceFilterCallback nextServiceFilterCallback) {
+
+            final SettableFuture<ServiceFilterResponse> resultFuture = SettableFuture.create();
+
+
+            runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    if (mProgressBar != null) mProgressBar.setVisibility(ProgressBar.VISIBLE);
+                }
+            });
+
+            ListenableFuture<ServiceFilterResponse> future = nextServiceFilterCallback.onNext(request);
+
+            Futures.addCallback(future, new FutureCallback<ServiceFilterResponse>() {
+                @Override
+                public void onFailure(Throwable e) {
+                    resultFuture.setException(e);
+                }
+
+                @Override
+                public void onSuccess(ServiceFilterResponse response) {
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            if (mProgressBar != null) mProgressBar.setVisibility(ProgressBar.GONE);
+                        }
+                    });
+
+                    resultFuture.set(response);
+                }
+            });
+
+            return resultFuture;
+        }
     }
 
     public void increment(){
@@ -282,10 +384,34 @@ public class MainActivity extends AppCompatActivity {
         builder.setMessage(R.string.order_dialog_msg);
         builder.setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
+                Long tsLong = System.currentTimeMillis()/1000;
+                String mIsHotDog = tsLong.toString();
 
-
-                Toast.makeText(MainActivity.this, String.format("%.2f",mTotalPrice) + " €",
+                HotDogItem hotDogItem = new HotDogItem();
+                hotDogItem.setHotdog(mIsHotDog);
+                hotDogItem.setBbqSauce(mIsBbqSauce);
+                hotDogItem.setKetchup(mIsKetchup);
+                hotDogItem.setMayonnaise(mIsMayonnaise);
+                hotDogItem.setCurry(mIsCurry);
+                hotDogItem.setOnion(mIsOnion);
+                hotDogItem.setCheese(mIsCheese);
+                hotDogItem.setTotalPrice(mTotalPrice);
+                double i = hotDogItem.getTotalPrice();
+                Toast.makeText(MainActivity.this, String.format("%.2f", i) + " €",
                         Toast.LENGTH_SHORT).show();
+
+                mHotDogTable.insert(hotDogItem, new TableOperationCallback<HotDogItem>() {
+                    public static final String TAG = "TAG";
+
+                    @Override
+                    public void onCompleted(HotDogItem entity, Exception exception, ServiceFilterResponse response) {
+                        if (exception == null){
+                            Log.i(TAG, "Azure insert succeeded ID: " + entity);
+                        } else {
+                            Log.i(TAG, "Azure insert failed again " + exception);
+                        }
+                    }
+                });
 
             }
         });
